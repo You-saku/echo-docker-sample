d11ebf599aa357ad09a041f7c60eef65d03675a4
You-saku <yousaku.dev19@gmail.com>
2022-11-27 15:49:23 +0900

    delete: コメントアウト部分

--- server/main.go
+++ server/main.go
@@ -3,13 +3,11 @@ package main
 import (
 	// 自作packageのimport
 	// go.modにあるmodule名+パス(ハマったポイント)
-//	"app/server/models" // DB操作一覧
 	"app/server/env" // .envファイルを読み込む
 	"app/server/route" // ルーティングの定義
 )
 
 func main() {
 	env.Load() // 関数名は大文字じゃないと呼び出せない(ハマるポイント)
-//	models.Migrate()
 	route.Routing()
 }
